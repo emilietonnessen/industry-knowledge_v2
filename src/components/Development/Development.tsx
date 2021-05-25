e1413583f109d87fd87975586f28099399c8e644
Emizzari <emilie@tonnessen.com>
2021-05-25 10:13:12 +0200

    Update Development.tsx

--- src/components/Development/Development.tsx
+++ src/components/Development/Development.tsx
@@ -1,4 +1,4 @@
-import Courses from '../../components/Courses/Courses';
+import Courses from '../Courses/Courses';
 import Education from './Educations';
 import WorkExperience from './WorkExperiences';
 import Tools from './Tools';
